54ae780a4852f92835e6a4def0844f4c8512ddbb
Victor Augusto <vaas@icomp.ufam.edu.br>
2019-06-16 21:06:42 -0400

    Criados os models curso e jogada

---
+++ api/models/Curso.js
@@ -0,0 +1,34 @@
+/**
+ * Curso.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    id: {
+      autoIncrement: true,
+      unique: true,
+      type: 'number'
+    },
+    nome: {
+      required: true,
+      type: 'string',
+      maxLength: 64
+    },
+    sigla: {
+      type: 'string',
+      required: true,
+      columnType: 'char',
+      maxLength: 4
+    },
+    descricao: {
+      type: 'string',
+      columnType: 'text'
+    }
+  },
+
+};
+
---
+++ api/models/Jogada.js
@@ -0,0 +1,32 @@
+/**
+ * Jogada.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    id: {
+      autoIncrement: true,
+      unique: true,
+      type: 'number'
+    },
+    jogador: {
+      type: 'number',
+      required: true
+    },
+    pontuacao: {
+      type: 'number',
+      required: true
+    },
+    data: {
+      type: 'string',
+      required: true,
+      maxLength: 45
+    }
+  },
+
+};
+
